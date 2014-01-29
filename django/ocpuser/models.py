fc6864f4663acc1240af7f795b8fd31bf926191b
Priya <priya@austen.(none)>
2014-01-29 16:55:23 -0500

    Updated Django models for ocpuser with new project configuration schema

--- django/ocpuser/models.py
+++ django/ocpuser/models.py
@@ -2,11 +2,31 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
-class ocpProject ( models.Model):
-    token  =  models. CharField(max_length=200)
-    description  =  models. CharField(max_length=4096)
-    project  =  models. CharField(max_length=200)
-    dataset  =  models. CharField(max_length=200)
+# This model represent and creates an ocp dataset( database)
+class datasets ( models.Model):
+    dataset  =  models. CharField(max_length=200, primary_key=True,verbose_name="Name of the Image dataset")    
+    ximagesize =  models.IntegerField()
+    yimagesize =  models.IntegerField()
+    startslice = models.IntegerField()
+    endslice = models.IntegerField()
+    zoomlevels = models.IntegerField()
+    zscale = models.FloatField()
+    dataset_description  =  models. CharField(max_length=4096,blank=True)
+    
+    class Meta:
+        """ Meta """
+        # Required to override the default table name
+        db_table = u"datasets"
+    def __unicode__(self):
+        return self.name
+
+
+# This model represent and creates an ocp project( database)
+# It has a foreign key dependency on the datasets class
+class projects ( models.Model):
+    project  =  models. CharField(max_length=200, primary_key=True)
+    project_description  =  models. CharField(max_length=4096, blank=True)
+    dataset  =  models.ForeignKey(datasets)
 
     DATATYPE_CHOICES = (
         (1, 'Image'),
@@ -15,16 +35,8 @@ class ocpProject ( models.Model):
         (4, 'Channel 8 bit'),
         )
     datatype = models.IntegerField(choices=DATATYPE_CHOICES, default=1)
-    
-
-#    dataurl  =  models. CharField(max_length=200)
+    dataurl  =  models.CharField(max_length=200)
     resolution = models.IntegerField(default=0)
-    READONLY_CHOICES = (
-        (1, 'Yes'),
-        (0, 'No'),
-        )
-    readonly =  models.IntegerField(choices=READONLY_CHOICES, default=2)
-
     EXCEPTION_CHOICES = (
         (1, 'Yes'),
         (0, 'No'),
@@ -35,6 +47,10 @@ class ocpProject ( models.Model):
         ('braingraph1.cs.jhu.edu', 'openconnecto.me'),
         ('braingraph1dev.cs.jhu.edu', 'braingraph1dev'),
         ('braingraph2.cs.jhu.edu', 'braingraph2'),
+        ('dsp029.pha.jhu.edu', 'Datascope 029'),
+        ('dsp061.pha.jhu.edu', 'Datascope 061'),
+        ('dsp062.pha.jhu.edu', 'Datascope 062'),
+        ('dsp063.pha.jhu.edu', 'Datascope 063'),
         )
     host =  models.CharField(max_length=200, choices=HOST_CHOICES, default='localhost')
 #    NOCREATE_CHOICES = (
@@ -45,26 +61,31 @@ class ocpProject ( models.Model):
 
     class Meta:
         """ Meta """
-        app_label = 'emca'
+        # Required to override the default table name
         db_table = u"projects"
     def __unicode__(self):
         return self.name
 
-# Create your models here.
-class ocpDataset ( models.Model):
-    dataset  =  models. CharField(max_length=200)    
-    ximagesize =  models.IntegerField()
-    yimagesize =  models.IntegerField()
 
-    startslice = models.IntegerField()
-    endslice = models.IntegerField()
-    zoomlevels = models.IntegerField()
-    zscale = models.FloatField()
-    description  =  models. CharField(max_length=4096)
+class tokens ( models.Model):
+    token  =  models. CharField(max_length=200, primary_key=True)
+    token_description  =  models. CharField(max_length=4096,blank=True)
+    project  = models.ForeignKey(projects)
+    READONLY_CHOICES = (
+        (1, 'Yes'),
+        (0, 'No'),
+        )
+    readonly =  models.IntegerField(choices=READONLY_CHOICES, default=2)
+    PUBLIC_CHOICES = (
+        (1, 'Yes'),
+        (0, 'No'),
+        )
+    public =  models.IntegerField(choices=PUBLIC_CHOICES, default=2)
+    
     
     class Meta:
         """ Meta """
-        app_label = 'emca'
-        db_table = u"projects"
+         # Required to override the default table name
+        db_table = u"tokens"
     def __unicode__(self):
         return self.name
